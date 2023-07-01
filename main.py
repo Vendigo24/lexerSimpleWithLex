02e258910a39c22807f1245a8bb1d99c688e6dcf
Шамиль <mamedov.shamil14@gmail.com>
2023-07-01 17:37:43 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,118 @@
+import ply.lex as lex
+import ply.yacc as yacc
+
+# Define a list of token names
+tokens = (
+    'NAME',
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN'
+)
+
+# Define regular expressions for each token
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_TIMES = r'\*'
+t_DIVIDE = r'\/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_NAME = r'[a-zA-Z][a-zA-Z0-9_.]*'
+
+
+def t_NUMBER(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
+    return t
+
+
+t_ignore = " \t"
+
+
+def t_NUMBER_int(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
+
+
+def p_expression_plus(p):
+    'expression : expression PLUS term'
+    p[0] = p[1] + p[3]
+
+
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    p[0] = p[1] - p[3]
+
+
+def p_expression_term(p):
+    'expression : term'
+    p[0] = p[1]
+
+
+def p_term_times(p):
+    'term : term TIMES factor'
+    p[0] = p[1] * p[3]
+
+
+def p_term_divide(p):
+    'term : term DIVIDE factor'
+    p[0] = p[1] / p[3]
+
+
+def p_term_factor(p):
+    'term : factor'
+    p[0] = p[1]
+
+
+def p_factor_num(p):
+    'factor : NUMBER'
+    p[0] = p[1]
+
+
+def p_factor_expr(p):
+    'factor : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+
+def p_factor_name(p):
+    'factor : NAME'
+    p[0] = p[1]
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
+
+
+# Build the parser
+parser = yacc.yacc()
+
+
+# Test the parser
+while True:
+    try:
+        s = input('calc > ')
+    except EOFError:
+        break
+    if not s:
+        continue
+    result = parser.parse(s)
+    print(result)
